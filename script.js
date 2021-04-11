2e0677a3933872358c7a7010179eff17e218be34
Kimberly Salas <salask0104@gmail.com>
2021-04-10 17:40:49 -0700

    added to the selected charaters true or false

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var lowercase = ["a", "b", "c", "d"]
-var uppercase = ["A", "B", "C", "D"]
-var num = [1, 2, 3, 4, 5]
-var symbols = [":", ";", "$", "/", "#"]
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"]
+var uppercase = ["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q"]
+var num = [1,2,3,4,5,6,7,8,9,0]
+var symbols = [":",";","$","/","#","(",")","%","?","<",">"]
+var masterCharacters = [ ];
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -16,22 +18,76 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var userInput = parseInt(prompt ("Select the length of your password"))
-  if (userInput < 8 || userInput > 128 || userInput == null) {
-    alert("Invalid input. Please choose a link between 8 to 128 characters.")
+  var userInput = parseInt(prompt ("Select the length of your password."))
+  if (userInput < 8 || userInput > 128 || userInput === null || userInput === "") {
+    alert("Invalid input. Please choose a valid length between 8 to 128 characters.")
     return;
   }
+  else {
+    console.log("Great. That length works")
+  }
 
+
+//asking what they want in their password
   var selectLowercase = confirm("Would you like lowercase characters?") 
   var selectUppercase = confirm("Would you like upperercase characters?")
   var selectNumeric = confirm("Would you like a numeric characters?")
   var selectSymbols = confirm("Would you like special characters?")
+
   if(selectLowercase === false && selectUppercase === false && selectNumeric === false && selectSymbols === false) {
     alert("Please select one type of character.")
     return;
   }
 
+//when they select an option they get a password with "that" character
+  if(selectLowercase === true) {
+    alert("You have selected lowercase characters.");
+    masterCharacters.push (lowercase)
+    console.log("master is" +masterCharacters)
+  }
+  else {
+    alert("No lowercase")
+  }
+
+  if (selectUppercase) {
+    alert("You have selected uppercase characters.");
+    masterCharacters.push (uppercase)
+    console.log("master is" +masterCharacters)
+  }
+  else{
+    alert("No uppercase.")
+  }
+  if (selectNumeric) {
+    alert("You have selected numeric characters.");
+    masterCharacters.push (num)
+    console.log("master is" +masterCharacters)
+    
+  }
+
+
+  if (selectSymbols) {
+    alert("You have selected special characters.");
+    masterCharacters.push (symbols)
+    console.log("master is" +masterCharacters)
+    
+  }
+
 }
+
+
+//verify if the password is valid
+function verifyPassword() {
+  var generatePassword = document.getElementById("password").value;
+
+  if (generatePassword == "") {
+    document.getElementById("message").innerHTML = "**Fill the password please!";
+    return false;
+  }
+}
+
+//display the password
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
