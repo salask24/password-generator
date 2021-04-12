f2568faea8afabb151b19d0e8d8e0782187969d3
Kimberly Salas <salask0104@gmail.com>
2021-04-11 17:44:43 -0700

    added for loop, refactored code and more to strings

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"]
-var uppercase = ["A","B","C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q"]
-var num = [1,2,3,4,5,6,7,8,9,0]
-var symbols = [":",";","$","/","#","(",")","%","?","<",">"]
-var masterCharacters = [ ];
-
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var symbols = [":", ";", "$", "/", "#", "(", ")", "%", "?", "<", ">","~","`","_","-","=","+","@"];
+var masterCharacters = [];
+var passwordText = document.querySelector("#password");
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function generatePassword() {
+
+  var userInput = parseInt(prompt("Select the length of your password."))
+  if (userInput < 8 || userInput > 128 || userInput === null || userInput === "") {
+    alert("Invalid input. Please choose a valid length between 8 to 128 characters.")
+    return;
+  }
+
+  //asking what they want in their password
+  var selectLowercase = confirm("Would you like lowercase characters?")
+  var selectUppercase = confirm("Would you like upperercase characters?")
+  var selectNumeric = confirm("Would you like a numeric characters?")
+  var selectSymbols = confirm("Would you like special characters?")
+
+  while (selectLowercase === false && selectUppercase === false && selectNumeric === false && selectSymbols === false) {
+    alert("Please select one type of character.")
+    selectLowercase = confirm("Would you like lowercase characters?")
+    selectUppercase = confirm("Would you like upperercase characters?")
+    selectNumeric = confirm("Would you like a numeric characters?")
+    selectSymbols = confirm("Would you like special characters?")
+  }
+
+  //true or false with selecting 
+  if (selectLowercase) {
+    masterCharacters.push(lowercase)
+  }
+
+  //uppercase
+  if (selectUppercase) {
+    masterCharacters.push(uppercase)
+  }
+
+  //numbers
+  if (selectNumeric) {
+    masterCharacters.push(num)
+  }
+
+  //symbols
+  if (selectSymbols) {
+    masterCharacters.push(symbols)
+  }
+
+  var password = "";
+
+  for (i = 0; i < userInput; i++) {
+
+    var randomArr = masterCharacters[Math.floor(Math.random() * masterCharacters.length)];
+    var randomChar = randomArr[Math.floor(Math.random() * randomArr.length)];
+    password += randomChar;
+
+  }
+  return password;
+
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -16,103 +72,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-function generatePassword() {
-  var userInput = parseInt(prompt ("Select the length of your password."))
-  if (userInput < 8 || userInput > 128 || userInput === null || userInput === "") {
-    alert("Invalid input. Please choose a valid length between 8 to 128 characters.")
-    return;
-  }
-  else {
-    console.log("Great. That length works")
-  }
-
-
-//asking what they want in their password
-  var selectLowercase = confirm("Would you like lowercase characters?") 
-  var selectUppercase = confirm("Would you like upperercase characters?")
-  var selectNumeric = confirm("Would you like a numeric characters?")
-  var selectSymbols = confirm("Would you like special characters?")
-
-  if(selectLowercase === false && selectUppercase === false && selectNumeric === false && selectSymbols === false) {
-    alert("Please select one type of character.")
-    return;
-  }
-
-//true or false with selecting 
-  if(selectLowercase === true) {
-    alert("You have selected lowercase characters.");
-    masterCharacters.push (lowercase)
-    console.log("master is" +masterCharacters)
-  }
-  else {
-    alert("No lowercase")
-  }
-//uppercase
-  if (selectUppercase) {
-    alert("You have selected uppercase characters.");
-    masterCharacters.push (uppercase)
-    console.log("master is" +masterCharacters)
-  }
-  else{
-    alert("No uppercase.")
-  }
-
-//numbers
-  if (selectNumeric) {
-    alert("You have selected numeric characters.");
-    masterCharacters.push (num)
-    console.log("master is" +masterCharacters)
-    
-  }
-  else {
-    alert("No numbers.")
-  }
-
- //symbols
-  if (selectSymbols) {
-    alert("You have selected special characters.");
-    masterCharacters.push (symbols)
-    console.log("master is" +masterCharacters)
-  }
-
-  else {
-    alert("No special characters.")
-  }
-
-  //for loop
-  var pass_array = [];
-
-    for(i = 0; i < length; i++){
-      var upper_final = Math.floor(Math.random() * uppercase.length + 1);
-      var lower_final = Math.floor(Math.random() * lowercase.length + 1);
-      var num_final = Math.floor(Math.random() * num.length + 1);
-      var special_final = Math.floor(Math.random() * symbols.length + 1);
-
-      if(selectUppercase === true) {
-        pass_array.push(uppercase);
-      }
-
-      if(selectLowercase === true){
-        pass_array.push(lower_final);
-      }
-
-      if(selectNumeric === true){
-        pass_array.push(num_final);
-      }
-
-      if(selectSymbols === true){
-        pass_array.push(special_final);
-      }
-      
-      return pass_array;
-    }
-    
-
-}
-
-//display the password
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
